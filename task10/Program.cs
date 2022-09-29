4d7587f69556ce1e2bd25e4ee666e8212caa317d
Semyon <tonkih.sem@mail.ru>
2022-09-29 20:04:53 +0500

    add task 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -5,3 +5,15 @@
  782 -> 72 
  918 -> 98
 */
+
+int number = new Random().Next(100,1000);
+
+void getDeleteTwoDigitFromNumber(int number)
+{
+    int firstNumber = number / 100;
+    int thirdNumber = number % 10;
+    Console.Write(firstNumber);
+    Console.Write(thirdNumber);
+}
+Console.WriteLine(number);
+getDeleteTwoDigitFromNumber(number);
