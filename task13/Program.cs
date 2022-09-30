aa9a0a4d0dc636745084787eb1317b5de3735ac3
Semyon <tonkih.sem@mail.ru>
2022-09-30 20:22:58 +0500

    Added task 13

---
+++ task13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого. 
+5, 25 -> да 
+-4, 16 -> да 
+25, 5 -> да 
+8,9 -> нет
+*/
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+string SquareOrNot(int numberA, int numberB)
+{
+    string YesOrNo = "";
+    if (numberA / numberB == numberB || numberB / numberA == numberA)
+    {
+        YesOrNo = "Да";
+    }
+    else YesOrNo = "Нет";
+    return YesOrNo;
+}
+string result = SquareOrNot(numberA, numberB);
+Console.WriteLine(result);
