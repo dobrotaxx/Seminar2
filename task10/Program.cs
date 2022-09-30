1d9a090acc7a7051b1a9e98e25dd2e408ca7b7bd
Semyon <tonkih.sem@mail.ru>
2022-09-30 18:42:34 +0500

    added change to task 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -8,12 +8,12 @@
 
 int number = new Random().Next(100,1000);
 
-void getDeleteTwoDigitFromNumber(int number)
+int getDeleteTwoDigitFromNumber(int number)
 {
     int firstNumber = number / 100;
     int thirdNumber = number % 10;
-    Console.Write(firstNumber);
-    Console.Write(thirdNumber);
+    return (firstNumber * 10 + thirdNumber);
 }
 Console.WriteLine(number);
-getDeleteTwoDigitFromNumber(number);
+int result = getDeleteTwoDigitFromNumber(number);
+Console.WriteLine(result);
---
+++ task11/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая будет принимать на вход два числа и 
+выводить, является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, то программа выводит остаток от деления. 
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+*/
+
+Console.WriteLine("Введите два числа A: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите два числа B: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+ int result = firstNumber % secondNumber;
+if(result > 0)
+{
+    Console.WriteLine($"Не кратно, остаток {result}");
+}
+else
+{
+    Console.WriteLine("Кратно");
+}
