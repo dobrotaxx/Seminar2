11456f18a0545b7bf6c1f75d956804b64983d196
Semyon <tonkih.sem@mail.ru>
2022-09-30 20:52:37 +0500

    Added DZ01

---
+++ DZ01/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int TwoDigit(int number)
+{
+    int TwoDigits = number % 100;
+    int OneDigit = TwoDigits / 10;
+    return (OneDigit);
+}
+int result = TwoDigit(number);
+Console.WriteLine(result);
