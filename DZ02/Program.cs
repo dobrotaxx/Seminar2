05638be5123962a1b7e1052300acc7a1ab1dde1b
Semyon <tonkih.sem@mail.ru>
2022-10-01 17:00:59 +0500

    Added DZ02 (not comleted)

--- DZ02/Program.cs
+++ DZ02/Program.cs
@@ -9,4 +9,18 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int ThirdDigit
+int ThirdDigit(int number)
+{
+    if (number < 99)
+    {
+       Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        number = number % 100;
+        number = number % 10;
+        Console.WriteLine($"{number}");
+    }
+    return number;
+}
+ThirdDigit(number);
